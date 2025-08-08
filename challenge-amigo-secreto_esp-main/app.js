498dfd1c5e4225aea30701789c4e8e4bff2a6370
ezequiel merkier <ezenicomerkier04@gmail.com>
2025-08-08 19:48:27 -0300

    proyecto funcional

--- challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/app.js
@@ -1,1 +1,37 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo() {
+    const nombre = document.getElementById('amigo').value;
+
+    if (nombre === '') {
+        alert('Por favor, inserte un nombre');
+    } else {
+        amigos.push(nombre);
+        document.getElementById('amigo').value = '';
+        mostrarAmigos();
+    }
+}
+
+function mostrarAmigos() {
+    const lista = document.getElementById('listaAmigos');
+    lista.innerHTML = "";
+    
+    for (let i = 0; i < amigos.length; i++) {
+       
+        const li = document.createElement('li');
+        li.textContent = amigos[i]; 
+        lista.appendChild(li);
+    }
+}
+
+function sortearAmigo() {
+    if (amigos.length === 0) {
+        alert('No hay amigos para sortear.');
+    } else {
+    const indiceAleatorio = Math.floor(Math.random() * amigos.length);
+    const amigoSorteado = amigos[indiceAleatorio];
+    const resultado = document.getElementById('resultado');
+    resultado.innerHTML = `<li>${amigoSorteado}</li>`;
+    }
+}
